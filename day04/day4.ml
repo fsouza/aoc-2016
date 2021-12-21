4a787ead81740de0952dedfc0d29d58b68a8f8cb
francisco souza <fsouza@users.noreply.github.com>
2021-12-21 18:41:42 -0500

    day 4 part 2

--- day04/day4.ml
+++ day04/day4.ml
@@ -2,6 +2,7 @@ open Core
 module CharMap = Map.Make (Char)
 
 type room = { encrypted_name : string; checksum : string; sector_id : int }
+type decrypted_room = { name : string; sector_id : int }
 
 let line_re = Re.compile (Re.Pcre.re {|^(.+)-(\d+)\[([a-z]{5})\]$|})
 
@@ -36,12 +37,42 @@ let calc_checksum { encrypted_name; _ } =
 let is_valid ({ checksum; _ } as room) =
   String.equal (calc_checksum room) checksum
 
+let a_code = Char.to_int 'a'
+let int_of_ch ch = Char.to_int ch - a_code
+let ch_of_int v = Char.of_int_exn (v + a_code)
+
+let decrypt n ch =
+  if Char.is_alpha ch then
+    let v = int_of_ch ch in
+    let v = (v + n) mod 26 in
+    ch_of_int v
+  else ' '
+
+let decrypt { encrypted_name; sector_id; _ } =
+  let name = String.map ~f:(decrypt sector_id) encrypted_name in
+  { name; sector_id }
+
+let part1 rooms =
+  rooms
+  |> List.fold_left ~init:0 ~f:(fun acc ({ sector_id; _ } : room) ->
+         sector_id + acc)
+  |> Printf.printf "Part 1: %d\n"
+
+let part2 rooms =
+  Printf.printf "Part 2:\n";
+  rooms
+  |> List.map ~f:decrypt
+  |> List.iter ~f:(fun { name; sector_id } ->
+         Printf.printf "%s (%d)\n" name sector_id)
+
 let () =
-  In_channel.stdin
-  |> In_channel.fold_lines ~init:[] ~f:(fun rooms line ->
-         match parse_room line with
-         | None -> rooms
-         | Some room -> room :: rooms)
-  |> List.filter ~f:is_valid
-  |> List.fold_left ~init:0 ~f:(fun acc { sector_id; _ } -> sector_id + acc)
-  |> Printf.printf "%d\n"
+  let valid_rooms =
+    In_channel.stdin
+    |> In_channel.fold_lines ~init:[] ~f:(fun rooms line ->
+           match parse_room line with
+           | None -> rooms
+           | Some room -> room :: rooms)
+    |> List.filter ~f:is_valid
+  in
+  part1 valid_rooms;
+  part2 valid_rooms
