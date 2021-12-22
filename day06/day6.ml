b57406b389e86edfc1fc66e25e79fec04beaf956
francisco souza <fsouza@users.noreply.github.com>
2021-12-21 22:54:12 -0500

    day06: simplify a bit further

--- day06/day6.ml
+++ day06/day6.ml
@@ -27,28 +27,28 @@ let make_freqs lines =
   freqs
 
 let find_ecm_max_freq freqs =
-  let ecm =
-    freqs
-    |> Array.map
-         ~f:
-           (Hashtbl.fold ~init:('\x00', 0)
-              ~f:(fun ~key ~data (most_common, most_common_amount) ->
-                if data > most_common_amount then (key, data)
-                else (most_common, most_common_amount)))
-  in
-  ecm |> Array.to_list |> List.map ~f:fst |> String.of_char_list
+  freqs
+  |> Array.map
+       ~f:
+         (Hashtbl.fold ~init:('\x00', 0)
+            ~f:(fun ~key ~data (most_common, most_common_amount) ->
+              if data > most_common_amount then (key, data)
+              else (most_common, most_common_amount)))
+  |> Array.to_list
+  |> List.map ~f:fst
+  |> String.of_char_list
 
 let find_ecm_min_freq freqs =
-  let ecm =
-    freqs
-    |> Array.map
-         ~f:
-           (Hashtbl.fold ~init:('\x00', Int.max_value)
-              ~f:(fun ~key ~data (least_common, least_common_amount) ->
-                if data < least_common_amount then (key, data)
-                else (least_common, least_common_amount)))
-  in
-  ecm |> Array.to_list |> List.map ~f:fst |> String.of_char_list
+  freqs
+  |> Array.map
+       ~f:
+         (Hashtbl.fold ~init:('\x00', Int.max_value)
+            ~f:(fun ~key ~data (least_common, least_common_amount) ->
+              if data < least_common_amount then (key, data)
+              else (least_common, least_common_amount)))
+  |> Array.to_list
+  |> List.map ~f:fst
+  |> String.of_char_list
 
 let () =
   let lines = In_channel.stdin |> In_channel.input_lines in
