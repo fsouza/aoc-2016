1531d49b8873af51d265c2da7312918be45f0bea
francisco souza <fsouza@users.noreply.github.com>
2021-12-25 19:44:28 -0500

    day 9 part 2

--- day09/day9.ml
+++ day09/day9.ml
@@ -23,8 +23,27 @@ let decompressed_length input =
   in
   decompressed_length 0 0
 
+let rec decompressed_length_v2 first_idx last_idx input =
+  let rec decompressed_length_v2' acc idx =
+    if idx = last_idx then acc
+    else
+      let ch = input.[idx] in
+      if Char.equal ch '(' then consume_marker acc idx
+      else decompressed_length_v2' (acc + 1) (idx + 1)
+  and consume_marker acc idx =
+    let closing_pos = String.index_from_exn input idx ')' in
+    let marker = String.sub ~pos:idx ~len:(closing_pos + 1 - idx) input in
+    let len, times = parse_marker marker in
+    let first_idx = closing_pos + 1 in
+    let last_idx = closing_pos + 1 + len in
+    let acc = acc + (times * decompressed_length_v2 first_idx last_idx input) in
+    decompressed_length_v2' acc last_idx
+  in
+  decompressed_length_v2' 0 first_idx
+
 let () =
-  In_channel.stdin
-  |> In_channel.input_line_exn
-  |> decompressed_length
-  |> Printf.printf "%d\n"
+  let input = In_channel.stdin |> In_channel.input_line_exn in
+  input |> decompressed_length |> Printf.printf "Part 1: %d\n";
+  input
+  |> decompressed_length_v2 0 (String.length input)
+  |> Printf.printf "Part 2: %d\n"
